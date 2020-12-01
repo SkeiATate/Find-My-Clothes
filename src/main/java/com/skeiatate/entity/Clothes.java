b496b957151a085baddd9f3f30c20fbfd9d33b0f
SkeiATate <skeiatate@163.com>
2020-12-01 08:48:41 +0800

    更新一下

--- src/main/java/com/skeiatate/entity/Clothes.java
+++ src/main/java/com/skeiatate/entity/Clothes.java
@@ -7,19 +7,23 @@ import java.util.List;
 
 /**
  * @author SkeiATate
- *
+ * <p>
  * 衣物实体类
  */
 @Entity
 @Data
-@Table(name="tb_clothes")
+@Table(name = "tb_clothes")
 public class Clothes {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
 
     @Column
-    @OneToMany(fetch = FetchType.EAGER)
+    // 衣物名称,后面用于模糊搜索
+    private String title;
+
+    @Column
+    @ManyToMany(fetch = FetchType.EAGER)
     //颜色描述
     private List<ClothesColor> colors;
 
